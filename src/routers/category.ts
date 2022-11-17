d4c9da20d9bdf56999b842e6f6cac3b2a3f0e6c4
Kambaia Alberto <kambaiaalberto@gmail.com>
2022-11-17 15:14:57 +0100

    edit a category router

--- src/routers/category.ts
+++ src/routers/category.ts
@@ -5,6 +5,9 @@ export const categoryRouter = Router();
 
 /********************************authintication ************************ */
 categoryRouter.post("/api/category", categoryController.saveCategory);
-categoryRouter.get("/api/categorys", categoryController.listAllCategory);
+categoryRouter.get("/api/categorys/", categoryController.listAllCategory);
+categoryRouter.get("/api/category/:categoryId", categoryController.listOneCategory);
+categoryRouter.put("/api/category/:categoryId", categoryController.updateCategory);
+categoryRouter.delite("/api/category/:categoryId", categoryController.deleteCategory);
 
 
